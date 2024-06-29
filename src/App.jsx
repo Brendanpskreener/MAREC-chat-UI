3219f455adcab41e01fbf1056d5be21e664d6462
Brendan Schreiner <brendanpschreiner@gmail.com>
2024-06-28 22:55:57 -0700

    trying AWS Amplify with React

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import './App.css'
+import { awsExports } from "./aws-exports"
+import { Amplify } from "aws-amplify"
+import { Authenticator } from "@aws-amplify/ui-react"
+import '@aws-amplify/ui-react/styles.css'
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID,
+      userPoolId: awsExports.USER_POOL_ID,
+    }
+  }
+})
+
 export default function App() {
 
   return (
-    <>
-      <div className="centered-form">
-        <div className="centered-form__box">
-          <h1>MAREC Chat</h1>
-          <button>Login</button>
-        </div>
-      </div>
-    </>
+    <Authenticator>
+      {({ signOut, user }) => {
+        <>
+          <p>Welcome {user.username}</p>
+          <button onClick={signOut}>Logout</button>
+        </>
+      }}
+    </Authenticator>
+    // <>
+    //   <div className="centered-form">
+    //     <div className="centered-form__box">
+    //       <h1>MAREC Chat</h1>
+    //       <button>Login</button>
+    //     </div>
+    //   </div>
+    // </>
   )
 }
 
---
+++ src/aws-exports.js
@@ -0,0 +1,5 @@
+export const awsExports = {
+  "REGION": "us-west-2",
+  "USER_POOL_ID": "us-west-2_V8jAMpVLR",
+  "USER_POOL_APP_CLIENT_ID": "4ojucaih0hkhva2k12mlth97qb"
+}
